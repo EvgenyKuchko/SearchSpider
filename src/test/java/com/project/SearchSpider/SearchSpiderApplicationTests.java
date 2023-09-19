1ec4fcdb136b69a7a1f624a8911978aac4c2074c
EvgenyKuchko <kuchkozhenia@gmail.com>
2023-09-19 15:32:40 +0200

    Initial commit

---
+++ src/test/java/com/project/SearchSpider/SearchSpiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SearchSpider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSpiderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
