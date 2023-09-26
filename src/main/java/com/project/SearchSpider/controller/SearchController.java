5abbe06e3f2d19455a3aab66a04642043e285546
EvgenyKuchko <kuchkozhenia@gmail.com>
2023-09-26 14:26:57 +0200

    added logging

--- src/main/java/com/project/SearchSpider/controller/SearchController.java
+++ src/main/java/com/project/SearchSpider/controller/SearchController.java
@@ -2,6 +2,7 @@ package com.project.SearchSpider.controller;
 
 import com.project.SearchSpider.model.SearchResult;
 import com.project.SearchSpider.service.SearchService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/search")
+@Slf4j
 public class SearchController {
 
     @Autowired
@@ -22,6 +24,7 @@ public class SearchController {
 
     @GetMapping()
     public String showSearchPage(Model model) {
+        log.info("The search page is displayed");
         List<SearchResult> searchResults = new ArrayList<>();
         model.addAttribute("searchResults", searchResults);
         return "search";
@@ -29,7 +32,9 @@ public class SearchController {
 
     @PostMapping()
     public String searchData(@RequestParam String query, Model model) {
+        log.info("Requested search results for the query: {}", query);
         List<SearchResult> searchResults = searchService.getData(query);
+        log.info("was obtained {} results for the query: {}", searchResults.size(), query);
         model.addAttribute("searchResults", searchResults);
         return "search";
     }
